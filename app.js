2f538944e81edadcb7a5bbb83c5fbadfecefcfe0
skshanto750099 <69714940+skshanto750099@users.noreply.github.com>
2020-08-15 17:49:30 +0600

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyCdPv4gxsEmNAnE_z9LhHwqWLdp5IvyBZ0",
+  authDomain: "hacking-c5b53.firebaseapp.com",
+  databaseURL: "https://hacking-c5b53.firebaseio.com",
+  projectId: "hacking-c5b53",
+  storageBucket: "hacking-c5b53.appspot.com",
+  messagingSenderId: "481513919792",
+  appId: "1:481513919792:web:75694a77716829720ba4f5",
+  measurementId: "G-S3782FLF8C"
 };
 
      
